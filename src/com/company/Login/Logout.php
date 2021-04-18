fcc736e551f7322e46ccac5ea579a1439cc57a93
Travis <82448667+cyberhog666@users.noreply.github.com>
2021-04-18 11:56:01 +1000

    Cleaned up login.php/logout.php

--- src/com/company/Login/Logout.php
+++ src/com/company/Login/Logout.php
@@ -1,13 +1,8 @@
 <?php
-
 session_start();
-
-//unset all session variables
-$SESSION = array();
 
 session_destroy();
 
-//redirct to login page -- i think this needs to be changed to a PHP file as well
-header("location: Login.html");
+header("location: Login.php");
 exit;
 ?>
