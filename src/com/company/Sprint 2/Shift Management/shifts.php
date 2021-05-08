5fc7a4b2aee8ef9c9d7ff0f6c8b8b9190514cc55
Quoc Pham <s3783358@student.rmit.edu.au>
2021-05-08 16:08:19 +1000

    Added Sprint 2 Directory
    Added Sprint 2 Directory

---
+++ src/com/company/Sprint 2/Shift Management/shifts.php
@@ -0,0 +1,26 @@
+<?php
+//using this page we store the data completed in AllocateShift.php into the database
+
+require_once "../db_config.php";
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
+$date = $_POST['date'];
+$start = $_POST['start'];
+$end = $_POST['end'];
+
+
+
+
+$sql = "INSERT INTO `tbl_shifts` (`Id`, `fldFirstname`, `fldLastname`, `fldDate`, `fldStart`, `fldEnd`) VALUES ('0', '$firstname', '$lastname', '$date', '$start', '$end')";
+
+
+
+if(mysqli_query($link, $sql))
+{
+    header("location: AllocateShift.php");
+    echo "Shift has been allocated";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($link);
+}
+
+?>
