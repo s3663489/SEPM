1be77b62c23b6478b1b4437323ad717dee933dfc
Quoc Pham <s3783358@student.rmit.edu.au>
2021-05-09 21:32:29 +1000

    Added in Van's code with status column coding included!
    Added in Van's code with status column included in shifts.php!

--- src/com/company/Sprint 2/Shift Management/shifts.php
+++ src/com/company/Sprint 2/Shift Management/shifts.php
@@ -1,7 +1,8 @@
 <?php
-//using this page we store the data completed in AllocateShift.php into the database
+/* using this page, we store the data completed in AllocateShift.php into the database */
 
-require_once "../db_config.php";
+$con = mysqli_connect("remotemysql.com","bVDXbaGLUd","f9i1PHT1zO","bVDXbaGLUd");
+
 $firstname = $_POST['firstname'];
 $lastname = $_POST['lastname'];
 $date = $_POST['date'];
@@ -9,18 +10,16 @@ $start = $_POST['start'];
 $end = $_POST['end'];
 
 
-
+$rs = mysqli_query($con, $sql);
 
 $sql = "INSERT INTO `tbl_shifts` (`Id`, `fldFirstname`, `fldLastname`, `fldDate`, `fldStart`, `fldEnd`, `fldStatus`) VALUES ('0', '$firstname', '$lastname', '$date', '$start', '$end', 'Pending')";
 
 
-
-if(mysqli_query($link, $sql))
+if($rs)
 {
-    header("location: AllocateShift.php");
-    echo "Shift has been allocated";
-} else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($link);
+	echo "Shift has been allocated";
 }
 
 ?>
+
+/* parts of this code were adopted and referenced from a tutorial from raghwendra.com */
